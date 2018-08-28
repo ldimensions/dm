504cae6e29c28edde5599efe9d01c07a680e26fc
Lindo Sebastian <lindosebastian@gmail.com>
2018-08-28 18:23:33 -0500

    changes

--- resources/views/related.blade.php
+++ resources/views/related.blade.php
@@ -17,7 +17,7 @@
                 @endif  
             </div> 
             <div class="content1"> 
-                <a href="../{{config('app.defaultBaseURL.grocery-store-details')}}/{{ $rel['urlName'] }}" class="title"><h1 class="colorh1">{{ $rel['name'] }}</h1></a>
+                <a href="../{{config('app.defaultBaseURL.grocery-store-details')}}/{{ $rel['urlName'] }}" title="{{$rel['name']}}" class="title"><h1 class="colorh1">{{ $rel['name'] }}</h1></a>
             </div>
             <a href="https://www.google.com/maps/dir/{{$rel['latitude']}},{{$rel['longitude']}}" target="_blank" class="mapicon"><img src="{{ URL::to('/') }}/image/map1.svg" alt="{{$loop->index}}{{ $rel['name'] }}"/></a>
             <div class="content2"> <span class="subcontent2">{{ $rel['address1'] }} {{ $rel['address2'] }}, {{ $rel['city'] }}, {{ $rel['state'] }} {{ $rel['zip'] }}</span> </div>
@@ -34,16 +34,11 @@
                 @endif   
             </div>              
             <div class="content1"> 
-                <a href="../{{config('app.defaultBaseURL.dallas-indian-restaurant')}}/{{ $rel['urlName'] }}" class="title"><h1 class="colorh1">{{ $rel['name'] }}<h1></a>
+                <a href="../{{config('app.defaultBaseURL.dallas-indian-restaurant')}}/{{ $rel['urlName'] }}" title="{{$rel['name']}}" class="title"><h1 class="colorh1">{{ $rel['name'] }}<h1></a>
             </div>
-            <a href="https://www.google.com/maps/dir/{{$rel['latitude']}},{{$rel['longitude']}}" target="_blank" class="mapicon"><img src="{{ URL::to('/') }}/image/map1.svg" alt="{{$loop->index}}{{ $rel['name'] }}"/></a>
-            <div class="content2"> <span class="subcontent1">Address:</span> <span class="subcontent2">{{ $rel['address1'] }} {{ $rel['address2'] }}, {{ $rel['city'] }}, {{ $rel['state'] }} {{ $rel['zip'] }}</span> </div>
-            <div class="content3"> <span class="subcontent1">Phone:</span> <span class="subcontent2"><a href="tel:{{ $rel['phone1'] }}">{{ $rel['phone1'] }}</a></span> </div>
-            <div class="content3"> <span class="subcontent1">Located In:</span> <span class="subcontent2">{{ $rel['city'] }}</span> </div>
-            @if (isset($rel['distance']) && $rel['distance'])
-                <div class="gro_kmblock_list">Distance : {{ $rel['distance'] }}</div>
-            @endif                 
-            <div class="open_close">Closed - Open 7 AM----</div>
+            <a href="https://www.google.com/maps/dir/{{$rel['latitude']}},{{$rel['longitude']}}" title="{{$rel['name']}}" target="_blank" class="mapicon"><img src="{{ URL::to('/') }}/image/map1.svg" alt="{{$loop->index}}{{ $rel['name'] }}"/></a>
+            <div class="content2"> <span class="subcontent2">{{ $rel['address1'] }} {{ $rel['address2'] }}, {{ $rel['city'] }}, {{ $rel['state'] }} {{ $rel['zip'] }}</span> </div>
+            <div class="content3"> <span class="subcontent2"><a href="tel:{{ $rel['phone1'] }}">{{ $rel['phone1'] }}</a></span> </div>            
         @endif 
         
         @if(isset($type) && $type == 'religion')
@@ -56,25 +51,20 @@
             </div>                
                 <div class="content1"> 
                     @if ($rel['religionName'] == 'Christianity')
-                        <a href="{{ URL::to('/') }}/{{config('app.defaultBaseURL.dallas-christian-church')}}/{{ $rel['urlName'] }}" class="title"><h1 class="colorh1">{{ $rel['name'] }}<h1></a>
+                        <a href="{{ URL::to('/') }}/{{config('app.defaultBaseURL.dallas-christian-church')}}/{{ $rel['urlName'] }}" title="{{$rel['name']}}" class="title"><h1 class="colorh1">{{ $rel['name'] }}<h1></a>
                     @elseif($rel['religionName'] == 'Hinduism')
-                        <a href="{{ URL::to('/') }}/{{config('app.defaultBaseURL.dallas-hindu-temple')}}/{{ $rel['urlName'] }}" class="title"><h1 class="colorh1">{{ $rel['name'] }}<h1></a>
+                        <a href="{{ URL::to('/') }}/{{config('app.defaultBaseURL.dallas-hindu-temple')}}/{{ $rel['urlName'] }}" title="{{$rel['name']}}" class="title"><h1 class="colorh1">{{ $rel['name'] }}<h1></a>
                     @elseif($rel['religionName'] == 'Judaism')
                         <!-- <a href="{{ URL::to('/') }}/{{config('app.defaultBaseURL.dallas-malayali-temple')}}/{{ $rel['urlName'] }}" class="title"><h1 class="colorh1">{{ $rel['name'] }}<h1></a> -->
                     @elseif($rel['religionName'] == 'Buddhism')
                         <!-- <a href="{{ URL::to('/') }}/{{config('app.defaultBaseURL.dallas-malayali-temple')}}/{{ $rel['urlName'] }}" class="title"><h1 class="colorh1">{{ $rel['name'] }}<h1></a> -->
                     @elseif($rel['religionName'] == 'Islam')
-                        <a href="{{ URL::to('/') }}/{{config('app.defaultBaseURL.dallas-islan-mosque')}}/{{ $rel['urlName'] }}" class="title"><h1 class="colorh1">{{ $rel['name'] }}<h1></a>                                                        
+                        <a href="{{ URL::to('/') }}/{{config('app.defaultBaseURL.dallas-islan-mosque')}}/{{ $rel['urlName'] }}" title="{{$rel['name']}}" class="title"><h1 class="colorh1">{{ $rel['name'] }}<h1></a>                                                        
                     @endif
                 </div>
-                <a href="https://www.google.com/maps/dir/{{$rel['latitude']}},{{$rel['longitude']}}" target="_blank" class="mapicon"><img src="{{ URL::to('/') }}/image/map1.svg" alt="{{$loop->index}}{{ $rel['name'] }}"/></a>
-                <div class="content2"> <span class="subcontent1">Address:</span> <span class="subcontent2">{{ $rel['address1'] }} {{ $rel['address2'] }}, {{ $rel['city'] }}, {{ $rel['state'] }} {{ $rel['zip'] }}</span> </div>
-                <div class="content3"> <span class="subcontent1">Phone:</span> <span class="subcontent2"><a href="tel:{{ $rel['phone1'] }}">{{ $rel['phone1'] }}</a></span> </div>
-                <div class="content3"> <span class="subcontent1">Located In:</span> <span class="subcontent2">{{ $rel['city'] }}</span> </div>
-                @if (isset($rel['distance']) && $rel['distance'])
-                    <div class="gro_kmblock_list">Distance : {{ $rel['distance'] }}</div>
-                @endif                 
-                <div class="open_close">Closed - Open 7 AM----</div>              
+                <a href="https://www.google.com/maps/dir/{{$rel['latitude']}},{{$rel['longitude']}}" title="{{$rel['name']}}" target="_blank" class="mapicon"><img src="{{ URL::to('/') }}/image/map1.svg" alt="{{$loop->index}}{{ $rel['name'] }}"/></a>
+                <div class="content2"> <span class="subcontent2">{{ $rel['address1'] }} {{ $rel['address2'] }}, {{ $rel['city'] }}, {{ $rel['state'] }} {{ $rel['zip'] }}</span> </div>
+                <div class="content3"> <span class="subcontent2"><a href="tel:{{ $rel['phone1'] }}">{{ $rel['phone1'] }}</a></span> </div>                
         @endif    
     </div>
     </div>
