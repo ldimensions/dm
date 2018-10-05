ac2db5c189a3580d1a216ab1b8d2f90af8d80797
lendel sebastian <lendel.sebastian@outlook.com>
2018-10-05 07:06:36 +0530

    your comment

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,8 @@
         <meta name="robots" content="index,follow" />
         {!! SEO::generate(true) !!}
 
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
